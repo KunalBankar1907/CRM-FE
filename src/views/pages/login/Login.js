3cce230fa412cd47e4cf5d0a9ed20fe978a99db8
Kunal Bankar <bankarkunal2016@gmail.com>
2026-01-29 12:47:25 +0530

    Logo adjust

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -94,7 +94,7 @@ const Login = () => {
                 <CCardBody>
                   <CForm onSubmit={handleSubmit}>
                     <div className='mx-auto text-center'>
-                      <CImage src={dummy_logo} width="100%"></CImage>
+                      <CImage src={dummy_logo} className='img-fluid' style={{ maxWidth: '15rem', width: '100%' }}></CImage>
                       {/* <h1 className='text-center'>Login</h1> */}
                       <p className="text-body-secondary text-center">Sign In to your account</p>
                     </div>
